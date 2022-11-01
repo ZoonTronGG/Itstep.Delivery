bbe5dffdfe339883a875aaf449c987a94cced9ee
МергазиевК <@223-17>
2022-11-01 21:59:28 +0600

    [add] city controller

---
+++ src/Itstep.Delivery.GlobalDictionary.Api/Controllers/CitiesController.cs
@@ -0,0 +1,32 @@
+﻿using Itstep.Delivery.GlobalDictionary.Api.Entities;
+using Itstep.Delivery.GlobalDictionary.Api.Persistance;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Itstep.Delivery.GlobalDictionary.Api.Controllers;
+
+[ApiController]
+[Route("api/cities")]
+public class CitiesController : ControllerBase
+{
+    private readonly DataContext _dbContext;
+
+    public CitiesController(DataContext dbContext)
+    {
+        _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+    }
+
+    [HttpGet("retrieve-cities-collection")]
+    public async Task<IActionResult> GetCitiesCollection()
+    {
+        var cities = await _dbContext.Cities.ToListAsync();
+        return Ok(cities);
+    }
+
+    [HttpGet("retrieve-city-by-id/{id}")]
+    public async Task<IActionResult> GetCityById(int id)
+    {
+        var cities = await _dbContext.Cities.FindAsync(id);
+        return Ok(cities);
+    }
+}
